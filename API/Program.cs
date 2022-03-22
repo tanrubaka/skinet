9eff57b843e77ae9b75bd7354362970a13c1fa3e
Evgeniy Nepomnyashchiy <tanrubaka@rambler.ru>
2022-03-22 21:15:02 +0300

    end of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhos:7249");
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
     });
 });
 
